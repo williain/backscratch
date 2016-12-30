7390c5d43a9d5fc2c9ce592d7297b8a0b8e5c1bb
williain <williain@users.noreply.github.com>
2016-12-30 16:13:23 +0000

    Improving handling of test link generation

--- test/inittest.py
+++ test/inittest.py
@@ -5,7 +5,7 @@ import os.path
 testfolder=os.path.join(os.path.dirname(os.path.abspath(__file__)),'data')
 
 def makeabsollink(linkfile, target):
-    if os.path.exists(os.path.join(testfolder,linkfile)):
+    if os.path.islink(os.path.join(testfolder,linkfile)):
         p=os.popen('readlink '+os.path.join(
           testfolder,linkfile
         ))
@@ -16,6 +16,11 @@ def makeabsollink(linkfile, target):
               curr_target, os.path.join(testfolder,target)
             ))
             os.unlink(os.path.join(testfolder, linkfile))
+    elif os.path.exists(os.path.join(testfolder, linkfile)):
+        print('{} exists as a non-link; please clean it up'.format(
+          os.path.join(testfolder, linkfile)
+        ))
+        exit(1)
     if not os.path.islink(os.path.join(testfolder, linkfile)):
         os.symlink(
           os.path.join(testfolder, target),
